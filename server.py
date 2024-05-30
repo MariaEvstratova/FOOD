ede7889c7065a48ab83b82edadbd46201b031bfe
MariaEvstratova <151522931+MariaEvstratova@users.noreply.github.com>
2024-05-30 23:35:59 +0700

    Update server.py

--- server.py
+++ server.py
@@ -202,6 +202,6 @@ def delete_item(id):
     return redirect('/basket')
 
 if __name__ == '__main__':
-    # app.run(port=8080, host='127.0.0.1', debug=True)
+    ## app.run(port=8080, host='127.0.0.1', debug=True) 
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
